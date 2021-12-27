1807d82b0598452495b9c37ed83251900c0e6e59
이용규 <yonggyu.lee@mirero.co.kr>
2021-12-27 17:51:02 +0900

    Lock 수정 중.

--- Mirero.DAQ.Test.Custom.Yglee.ApiService/Common/Lock/AdvisoryLock.cs
+++ Mirero.DAQ.Test.Custom.Yglee.ApiService/Common/Lock/AdvisoryLock.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Common;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Internal;
@@ -11,23 +13,80 @@ namespace Mirero.DAQ.Test.Custom.Yglee.ApiService.Common.Lock
     public class AdvisoryLock
     {
         private readonly AdvisoryLockKey _key;
+        private readonly bool _isShared;
 
-        public AdvisoryLock(AdvisoryLockKey key)
+
+        public AdvisoryLock(AdvisoryLockKey key, bool isShared = false)
         {
             _key = key;
+            _isShared = isShared;
         }
 
         public async ValueTask<object?> TryAcquire(DbContext context, TimeSpan timeout)
         {
-            await using (var cmd = context.Database.GetDbConnection().CreateCommand())
-            {
-                cmd.CommandText = "select * from dataset;";
-                cmd.CommandType = CommandType.Text;
+            await using var conn = context.Database.GetDbConnection();
+            await context.Database.OpenConnectionAsync();
 
-                await context.Database.OpenConnectionAsync();
+            using var acquireCommand = this.CreateAcquireCommand(conn);
+
+            int acquireCommandResult = 0;
+            try
+            {
+                acquireCommandResult = (int)await acquireCommand.ExecuteScalarAsync();
+            }
+            catch (Exception ex)
+            {
+                
             }
 
-            return 0;
+            return acquireCommandResult;
+        }
+
+        private DbCommand CreateAcquireCommand(DbConnection connection, TimeSpan timeout = new TimeSpan())
+        {
+            var cmd = connection.CreateCommand();
+
+            var cmdText = new StringBuilder();
+
+            cmdText.AppendLine("SET LOCAL statement_timeout = 0;");
+
+            cmdText.AppendLine($"SET LOCAL lock_timeout = {timeout.Milliseconds};");
+
+            cmdText.Append("SELECT");
+
+            cmdText.Append(" AS result");
+
+            cmd.CommandText = cmdText.ToString();
+            cmd.CommandTimeout = timeout.Milliseconds;
+
+            return cmd;
+
+            void AppendAcquireFunctionCall()
+            {
+                // creates an expression like
+                // pg_try_advisory_lock(@key1, @key2)::int
+                // OR (SELECT 1 FROM (SELECT pg_advisory_lock(@key)) f)
+                var isTry = timeout.Milliseconds == 0;
+                if (!isTry) { cmdText.Append("(SELECT 1 FROM (SELECT "); }
+                cmdText.Append("pg_catalog.pg");
+                if (isTry) { cmdText.Append("_try"); }
+                cmdText.Append("_advisory");
+                cmdText.Append("_lock");
+                if (this._isShared) { cmdText.Append("_shared"); }
+                cmdText.Append('(').Append(AddKeyParametersAndGetKeyArguments(cmd, _key)).Append(')');
+                if (isTry) { cmdText.Append("::int"); }
+                else { cmdText.Append(") f)"); }
+            }
+        }
+
+        private static string AddKeyParametersAndGetKeyArguments(DbCommand command, AdvisoryLockKey key)
+        {
+            var parameter = command.CreateParameter();
+            parameter.ParameterName = "key";
+            parameter.Value = key.Key;
+            parameter.DbType = DbType.Int64;
+            command.Parameters.Add(parameter);
+            return "@key";
         }
     }
 }
